708420769a0e6d0e430574379e5082e4152f6a76
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-09-20 12:19:39 +0900

    create project

---
+++ src/main/java/com/example/jsgamesbackendmain/JsGamesBackendMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsgamesbackendmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsGamesBackendMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsGamesBackendMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsgamesbackendmain/JsGamesBackendMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsgamesbackendmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsGamesBackendMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

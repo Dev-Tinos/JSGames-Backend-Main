2dc0ab78454ea816c5cff40de7dc2c27872a4041
woojin065 <marty0605@naver.com>
2023-09-20 11:42:52 +0900

    프로젝트 생성
    spring boot 생성
    - spring boot 2.7.x
    - java jdk 1.8

---
+++ Tinos-BackEnd/src/main/java/com/example/tinosbackend/TinosBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinosBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinosBackEndApplication.class, args);
+	}
+
+}

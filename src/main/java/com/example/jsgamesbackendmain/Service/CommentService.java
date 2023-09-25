a6ebb344b3de848ecd05fa0ccc593bf4c17bd938
woojin065 <marty0605@naver.com>
2023-09-25 11:35:18 +0900

    CommentService 생성

---
+++ src/main/java/com/example/jsgamesbackendmain/Service/CommentService.java
@@ -0,0 +1,42 @@
+package com.example.jsgamesbackendmain.Service;
+
+import com.example.jsgamesbackendmain.Bean.CommentBean.*;
+import com.example.jsgamesbackendmain.Model.DTO.Comment.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentCreateBean commentCreateBean;
+
+    @Autowired
+    private CommentListByGameBean commentListByGameBean;
+
+    @Autowired
+    private CommentUpdateBean commentUpdateBean;
+
+    @Autowired
+    private CommentDeleteBean commentDeleteBean;
+
+    // 댓글 작성
+    public CommentCreateResponseDTO createComment(CommentCreateRequestDTO requestDTO) {
+        return commentCreateBean.create(requestDTO);
+    }
+
+    // 게임 별 댓글 조회
+    public CommentListResponseDTO getCommentsByGame(Long gameId) {
+        return commentListByGameBean.getByGame(gameId);
+    }
+
+    // 댓글 수정
+    public CommentUpdateResponseDTO modifyComment(Long commentId, CommentUpdateRequestDTO requestDTO) {
+        return commentUpdateBean.modify(commentId, requestDTO);
+    }
+
+    // 댓글 삭제
+    public CommentDeleteResponseDTO removeComment(Long commentId) {
+        return commentDeleteBean.remove(commentId);
+    }
+}

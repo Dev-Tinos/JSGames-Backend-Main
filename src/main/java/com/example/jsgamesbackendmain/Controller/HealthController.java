a4776acd418583b46f0bfb7c27b57a6e7fb3829b
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-10-04 16:50:29 +0900

    cors 해제

--- src/main/java/com/example/jsgamesbackendmain/Controller/HealthController.java
+++ src/main/java/com/example/jsgamesbackendmain/Controller/HealthController.java
@@ -1,9 +1,11 @@
 package com.example.jsgamesbackendmain.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class HealthController {
     @GetMapping("/")
     public String healthCheck() {

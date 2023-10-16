4de345bcab88a04f48b5d974fd8cbb00241a4560
woojin065 <marty0605@naver.com>
2023-10-16 21:10:15 +0900

    이메일 인증 코드 작성 완료

--- src/main/java/com/example/jsgamesbackendmain/Controller/EmailController.java
+++ src/main/java/com/example/jsgamesbackendmain/Controller/EmailController.java
@@ -3,6 +3,7 @@ package com.example.jsgamesbackendmain.Controller;
 import com.example.jsgamesbackendmain.Model.DTO.Email.EmailCodeRequestDTO;
 import com.example.jsgamesbackendmain.Model.DTO.Email.EmailSendRequestDTO;
 import com.example.jsgamesbackendmain.Service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,5 +18,13 @@ public class EmailController {
     @PostMapping("/email")
     public String sendEmail(@RequestBody EmailSendRequestDTO emailRequestDTO) {
         return emailService.sendEmail(emailRequestDTO);
+    }
+
+    @Operation(summary = "이메일 코드 테이블 데이터 삭제", description =
+                    "## 또한 자동으로 오후 11시 55분 마다 자동으로 실행됨"
+    )
+    @PostMapping("/email/clear")
+    public String emailClear(){
+        return emailService.emailClear();
     }
 }

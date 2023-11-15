d9165f0cebf043d0d392d6aa2312e242cd2bbfd7
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-11-15 11:50:03 +0900

    DTO 수정

--- src/main/java/com/example/jsgamesbackendmain/Model/DTO/User/Reponse/UserSignUpResponseDTO.java
+++ src/main/java/com/example/jsgamesbackendmain/Model/DTO/User/Reponse/UserSignUpResponseDTO.java
@@ -1,6 +1,8 @@
 package com.example.jsgamesbackendmain.Model.DTO.User.Reponse;
 
 import com.example.jsgamesbackendmain.Model.DAO.UserDAO;
+import com.example.jsgamesbackendmain.Model.ENUM.Major;
+import com.example.jsgamesbackendmain.Model.ENUM.ParentMajor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,12 +11,14 @@ public class UserSignUpResponseDTO {
     private Long userId;
     private String nickname;
     private String email;
-    private String major;
+    private ParentMajor parentMajor;
+    private Major major;
     public static UserSignUpResponseDTO of(UserDAO userDAO) {
         UserSignUpResponseDTO userSignUpResponseDTO = new UserSignUpResponseDTO();
         userSignUpResponseDTO.setUserId(userDAO.getUserId());
         userSignUpResponseDTO.setNickname(userDAO.getNickname());
         userSignUpResponseDTO.setEmail(userDAO.getEmail());
+        userSignUpResponseDTO.setParentMajor(userDAO.getParentMajor());
         userSignUpResponseDTO.setMajor(userDAO.getMajor());
         return userSignUpResponseDTO;
     }
--- src/main/java/com/example/jsgamesbackendmain/Model/DTO/User/Reponse/UserUpdateResponseDTO.java
+++ src/main/java/com/example/jsgamesbackendmain/Model/DTO/User/Reponse/UserUpdateResponseDTO.java
@@ -2,6 +2,8 @@ package com.example.jsgamesbackendmain.Model.DTO.User.Reponse;
 
 
 import com.example.jsgamesbackendmain.Model.DAO.UserDAO;
+import com.example.jsgamesbackendmain.Model.ENUM.Major;
+import com.example.jsgamesbackendmain.Model.ENUM.ParentMajor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,13 +13,15 @@ public class UserUpdateResponseDTO {
     private String nickname;
     private String email;
     private String password;
-    private String major;
+    private ParentMajor parentMajor;
+    private Major major;
     public static UserUpdateResponseDTO of(UserDAO userDAO) {
         UserUpdateResponseDTO userUpdateResponseDTO = new UserUpdateResponseDTO();
         userUpdateResponseDTO.setUserId(userDAO.getUserId());
         userUpdateResponseDTO.setNickname(userDAO.getNickname());
         userUpdateResponseDTO.setEmail(userDAO.getEmail());
         userUpdateResponseDTO.setPassword(userDAO.getPassword());
+        userUpdateResponseDTO.setParentMajor(userDAO.getParentMajor());
         userUpdateResponseDTO.setMajor(userDAO.getMajor());
         return userUpdateResponseDTO;
     }

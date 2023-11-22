4e5d5d27b1d67256845ef15e37c20da6da641811
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-11-23 03:53:24 +0900

    s3 이미지 수정 로직 추가 및 수정할 데이터만 수정 로직 추가

--- src/main/java/com/example/jsgamesbackendmain/Bean/UserBean/UserUpdateBean.java
+++ src/main/java/com/example/jsgamesbackendmain/Bean/UserBean/UserUpdateBean.java
@@ -3,12 +3,15 @@ package com.example.jsgamesbackendmain.Bean.UserBean;
 import com.example.jsgamesbackendmain.Bean.MapperBean.MapperBean;
 import com.example.jsgamesbackendmain.Bean.SmallBean.UserBean.UserEmailDuplicateSmallBean;
 import com.example.jsgamesbackendmain.Bean.SmallBean.UserBean.UserGetSmallBean;
+import com.example.jsgamesbackendmain.Bean.SmallBean.UserBean.UserImageUpdateSmallBean;
 import com.example.jsgamesbackendmain.Model.DAO.UserDAO;
 import com.example.jsgamesbackendmain.Model.DTO.User.Reponse.UserUpdateResponseDTO;
 import com.example.jsgamesbackendmain.Model.DTO.User.Request.UserUpdateRequestDTO;
 import com.example.jsgamesbackendmain.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class UserUpdateBean {
@@ -25,14 +28,28 @@ public class UserUpdateBean {
     @Autowired
     private MapperBean mapperBean;
 
-    public UserUpdateResponseDTO updateUser(UserUpdateRequestDTO userUpdateRequestDTO) {
+    @Autowired
+    private UserImageUpdateSmallBean userImageUpdateBean;
+    public UserUpdateResponseDTO updateUser(UserUpdateRequestDTO userUpdateRequestDTO) throws IOException {
         userEmailDuplicateSmallBean.exec(userUpdateRequestDTO.getEmail());
 
         UserDAO user = userGetSmallBean.getUser(userUpdateRequestDTO.getUserId());
-        user.setNickname(userUpdateRequestDTO.getNickname());
-        user.setEmail(userUpdateRequestDTO.getEmail());
-        user.setMajor(userUpdateRequestDTO.getMajor());
-
+        if(userUpdateRequestDTO.getEmail() != null) {
+            user.setEmail(userUpdateRequestDTO.getEmail());
+        }
+        if(userUpdateRequestDTO.getNickname() != null) {
+            user.setNickname(userUpdateRequestDTO.getNickname());
+        }
+        if(userUpdateRequestDTO.getMajor() != null) {
+            user.setMajor(userUpdateRequestDTO.getMajor());
+        }
+        if(userUpdateRequestDTO.getImage() != null) {
+            user.setProfileImageURL(
+                    userImageUpdateBean.exec(
+                            userUpdateRequestDTO.getUserId(),
+                            userUpdateRequestDTO.getEmail(),
+                            userUpdateRequestDTO.getImage()));
+        }
         return mapperBean.to(userRepository.save(user), UserUpdateResponseDTO.class);
     }
 }

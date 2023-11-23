fc338bd7f289f910352b15f60de6c5a8756f007a
woojin065 <marty0605@naver.com>
2023-11-23 17:09:44 +0900

    HelpfulDAO 생성

--- src/main/java/com/example/jsgamesbackendmain/Model/DAO/HelpfulDAO.java
+++ src/main/java/com/example/jsgamesbackendmain/Model/DAO/HelpfulDAO.java
@@ -3,12 +3,16 @@ package com.example.jsgamesbackendmain.Model.DAO;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "helpful")
 @Getter
 @Setter
 public class HelpfulDAO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long helpfulId;
+    private String userId;
+    private Long reviewId;
 }

3b8f05c0f5a1412605a8af337ea4f7f1a0a05645
woojin065 <marty0605@naver.com>
2023-11-23 17:10:47 +0900

    ReviewDAO.helpful의 값 수정 로직 추가

--- src/main/java/com/example/jsgamesbackendmain/Repository/ReviewRepository.java
+++ src/main/java/com/example/jsgamesbackendmain/Repository/ReviewRepository.java
@@ -4,9 +4,24 @@ import com.example.jsgamesbackendmain.Model.DAO.ReviewDAO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewDAO, Long> {
     Page<ReviewDAO> findByGameIdOrderByDateTimeDesc(Long gameId, Pageable pageable);
+
+    //reviewID 의 helpful +1
+    @Modifying
+    @Transactional
+    @Query("UPDATE ReviewDAO r SET r.helpful = r.helpful + 1 WHERE r.reviewId = ?1")
+    void updateHelpfulPlus(Long reviewId);
+
+    //reviewID 의 helpful -1
+    @Modifying
+    @Transactional
+    @Query("UPDATE ReviewDAO r SET r.helpful = r.helpful - 1 WHERE r.reviewId = ?1")
+    void updateHelpfulMinus(Long reviewId);
 }

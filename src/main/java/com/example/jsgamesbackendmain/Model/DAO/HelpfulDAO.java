bdff4e7fce81ac7ada65a677b492ab20eec09e59
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-11-23 15:23:53 +0900

    helpfulDAO 생성

---
+++ src/main/java/com/example/jsgamesbackendmain/Model/DAO/HelpfulDAO.java
@@ -0,0 +1,14 @@
+package com.example.jsgamesbackendmain.Model.DAO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "helpful")
+@Getter
+@Setter
+public class HelpfulDAO {
+}

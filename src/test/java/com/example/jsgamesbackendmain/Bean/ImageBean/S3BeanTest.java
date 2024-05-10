c372bcd167f55a51a75bb284ab241039d198bd86
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-05-10 14:31:44 +0900

    imageBean 이름 수정

--- src/test/java/com/example/jsgamesbackendmain/Bean/ImageBean/S3BeanTest.java
+++ src/test/java/com/example/jsgamesbackendmain/Bean/ImageBean/S3BeanTest.java
@@ -2,10 +2,10 @@ package com.example.jsgamesbackendmain.Bean.ImageBean;
 
 import com.amazonaws.services.s3.AmazonS3;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest

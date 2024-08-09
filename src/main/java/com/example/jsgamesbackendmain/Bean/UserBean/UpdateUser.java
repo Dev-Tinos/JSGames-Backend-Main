51de5a0da69f6dac257fa367833a13c57d47835c
woojin065 <marty0605@naver.com>
2024-08-09 20:24:34 +0900

    유저정보수정 오류 수정

--- src/main/java/com/example/jsgamesbackendmain/Bean/UserBean/UpdateUser.java
+++ src/main/java/com/example/jsgamesbackendmain/Bean/UserBean/UpdateUser.java
@@ -24,7 +24,7 @@ public class UpdateUser {
     public UserUpdateResponseDTO exec(UserUpdateRequestDTO userUpdateRequestDTO) {
         UserDAO user = userGetByIdSmallBean.exec(userUpdateRequestDTO.getUserId());
 
-        if (userUpdateRequestDTO.getProfileImageURL() != null) {
+        if (userUpdateRequestDTO.getProfileImageURL() != null && user.getProfileImageURL() != "https://tinos-images-storage.s3.ap-northeast-2.amazonaws.com/default_user_image.png") {
             S3DeleteSmallBeam.exec(user.getProfileImageURL());
         }
 
